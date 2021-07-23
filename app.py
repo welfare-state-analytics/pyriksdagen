28c59826d9476f4c778ac2a76bd5397f399fa881
ninpnin <vainoyrjanainen@icloud.com>
2021-07-23 16:21:44 +0300

    Add example data for testing purposes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from pyriksdagen.utils import paragraph_iterator
 from pyriksdagen.utils import speeches_with_name
+from pyriksdagen.utils import parlaclarin_to_txt
 from lxml import etree
 
 filename = "example-data/prot-1949--ak--12.xml"
@@ -9,5 +10,12 @@ root = etree.parse(filename, parser).getroot()
 p_iter = paragraph_iterator(root)
 p_iter = speeches_with_name(root)
 
-for p in p_iter:
-    print(p)
+#for p in p_iter:
+#    print(p)
+
+filename = "example-data/prot-1949--ak--12.txt"
+txt = parlaclarin_to_txt(root)
+f = open(filename, "w")
+for line in txt.split("\n"):
+    f.write(line.strip() + "\n")
+f.close()
